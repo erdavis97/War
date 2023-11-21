38e353b532edabfec86ccfa56c82fae397beff8e
Ethan Davis <edavis@bsd220.org>
2023-11-21 10:01:31 -0600

    initial commit (title page)

--- War/ContentView.swift
+++ War/ContentView.swift
@@ -10,17 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            CustomText(text: "WAR")
+                .padding(55.0)
+            Image("aces")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Spacer()
         }
-        .padding()
+        .background(Rectangle().frame(width: 394.0, height: 2000.0) .foregroundColor(.mint))
+        .findDisabled()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CustomText: View {
+    let text: String
+    var body: some View {
+        Text(text).font(Font.custom("", size: 120))
     }
 }
