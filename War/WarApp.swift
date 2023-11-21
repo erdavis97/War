28b0f9b17bb17e136c1c5c0a3dcf62ccf9ef55d8
Ethan Davis <edavis@bsd220.org>
2023-11-21 09:29:48 -0600

    Initial Commit

---
+++ War/WarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarApp.swift
+//  War
+//
+//  Created by Ethan Davis on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct WarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
