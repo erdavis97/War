e4299611e751af689426678b5100041471054030
Ethan Davis <edavis@bsd220.org>
2023-11-28 10:08:42 -0600

    started work on game view

--- War/SinglePlayerView.swift
+++ War/SinglePlayerView.swift
@@ -9,9 +9,20 @@ import SwiftUI
 
 struct SinglePlayerView: View {
     var body: some View {
-        Text("filler")
+        VStack {
+            Image("gray_back")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 115.0, height: 180.0)
+                .padding()
+            Group {
+                Text("CPU has (filler) cards remaining ")
+                    .padding()
+                Text("Player 1 has (filler) cards remaining ")
+            }
+        }
     }
-        
+        //set image to top of screen, text to center
 }
 
 struct SinglePlayerView_Previews: PreviewProvider {
@@ -19,3 +30,4 @@ struct SinglePlayerView_Previews: PreviewProvider {
         SinglePlayerView()
     }
 }
+
