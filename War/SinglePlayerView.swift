59a1fdd16b5236b24303ab7c27be8da29f5965f0
Ethan Davis <edavis@bsd220.org>
2023-12-01 09:41:51 -0600

    added custom text and background

--- War/SinglePlayerView.swift
+++ War/SinglePlayerView.swift
@@ -8,41 +8,42 @@
 import SwiftUI
 
 struct SinglePlayerView: View {
-    @State private var remainingCards = ""
+    @State private var points = ""
     @State private var flipped = false
     var body: some View {
-        VStack {
-            ZStack {
-                Image("gray_back")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 100.0, height: 180.0)
-                    .offset(x: 0.0, y: -170.0)
-                if flipped {
-                    Image("KC")
+        ZStack {
+            Image("Green Background")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 300.0, height: 1000)
+            VStack {
+                ZStack {
+                    Image("gray_back")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 100.0, height: 180.0)
                         .offset(x: 0.0, y: -170.0)
+                    if flipped {
+                        Image("KC")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100.0, height: 180.0)
+                            .offset(x: 0.0, y: -170.0)
+                    }
                 }
+                //add button to flip, add animation
+                VStack {
+                    CustomText1(text: "CPU: \(points)")
+                        .offset(x: -145.0, y: 0.0)
+                    CustomText1(text: "Player 1: \(points)")
+                        .offset(x: -128.0, y: 0.0)
+                }
+                Image("blue_back")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100.0, height: 180.0)
+                    .offset(x: 0.0, y: 190.0)
             }
-            //add button to flip, add animation
-            HStack {
-                Text("CPU: \(remainingCards)")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.leading)
-                    .offset(x: -90.0, y: 0.0)
-                Text("Player 1: \(remainingCards)")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                    .offset(x: 90.0, y: 0.0)
-            }
-            Image("blue_back")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 100.0, height: 180.0)
-                .offset(x: 0.0, y: 190.0)
         }
     }
 }
@@ -52,4 +53,9 @@ struct SinglePlayerView_Previews: PreviewProvider {
         SinglePlayerView()
     }
 }
-
+struct CustomText1: View {
+    let text: String
+    var body: some View {
+        Text(text).font(Font.custom("", size: 36)).fontWeight(.bold).foregroundColor(Color.white)
+    }
+}
